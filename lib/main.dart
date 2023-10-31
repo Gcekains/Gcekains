389a26813c9bcdaec3033e45d6698d113ad52183
ATHUL BENNY <83030919+athulbenny@users.noreply.github.com>
2023-10-31 09:19:22 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:resumer/resumeChecker.dart';
+
+
+
+
+
+
+
+
+
+
 
 void main() {
   runApp(const MyApp());
@@ -10,40 +20,26 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Resumer',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Resumer'),
     );
   }
 }
 
+
+
+
+
+
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -52,42 +48,56 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  TextEditingController desc=new TextEditingController();
+  List data=[];
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-        appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: GestureDetector(
-            child: Container(height: 100,width: 100,color: Colors.black,
-              child: Icon(Icons.add,color: Colors.white,),
-            ),
-            onTap: ()async{
-              print("taped");
-              FilePickerResult? result= await FilePicker.platform.pickFiles();
-              if(result!=null) {
-                PlatformFile? file = result.files.first;
-                print(file.path);
-                final File fileFor = File(file.path??"");
-                fileFor.readAsString().asStream().listen((event) {
-                  print(event);
-                });
-                // List<File> files= result.paths.map((e) => File(e)).toList();
-              }
-            },
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children : [
+          GestureDetector(
+          child: Container(height: 100,width: 100,color: Colors.black,
+            child: Icon(Icons.add,color: Colors.white,),
           ),
+          onTap: ()async{
+            print("taped");
+            FilePickerResult? result= await FilePicker.platform.pickFiles();
+            if(result!=null) {
+              PlatformFile? file = result.files.first;
+              print(file.path);
+              final File fileFor = File(file.path??"");
+              data=[];
+              fileFor.readAsString().asStream().listen((event) {
+                data.add(event.toLowerCase());
+              });
+              // List<File> files= result.paths.map((e) => File(e)).toList();
+            }
+          },
+        ),
+          ElevatedButton(onPressed: (){
+            bool x= ResumeChecker().acceptOrReject(data, desc.text);
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("${x?'Agree':'Reject'}")));
+          }, child: Text("submit")),
+          Container(
+            child: Column(
+              children: [
+                TextField(
+                  autofocus: true,
+                  controller: desc,
+                ),
+                ElevatedButton(onPressed: (){
 
-        )// This trailing comma makes auto-formatting nicer for build methods.
+                }, child: Text("save"))
+              ],
+            ),
+          )
+
+      ]
+      )
     );
   }
-
 }
